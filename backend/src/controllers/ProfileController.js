24facc2e6adbac1e6c8c76684f78bb0a995d8d4c
HigorDenomar <higordenomar@hotmail.com>
2020-03-26 01:01:26 -0300

    correction in the list of cases

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,7 +1,7 @@
 const connection = require('../database/connection');
 module.exports = {
   async index(req, res) {
-    const ong_id = request.header.authorization;
+    const ong_id = req.headers.authorization;
 
     const incidents = await connection('incidents')
       .where('ong_id', ong_id)
